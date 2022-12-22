3fe15c1a716e484a95b61bea9fc6d5b38cc11dfe
Maria <mpriorova@mail.ru>
2022-12-22 22:10:04 +0300

    3 seminar

---
+++ Seminar3/1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input quadrant number: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine ("X>0; y>0");
+} 
+if (number == 2)
+{
+    Console.WriteLine ("x>0; y<0");
+}
+if (number == 3)
+{
+    Console.WriteLine ("x<0; y<0");
+}
+if (number == 4)
+{
+    Console.WriteLine ("x<0; y>0");
+}
+if (number < 1 || number > 4)
+{
+    Console.WriteLine ("input interger from 1 to 4");
+    return;
+}
